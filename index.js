49e3aa0c9464f0b033e74e2b5e0acf817b2e7be4
gracegerwe <86850911+gracegerwe@users.noreply.github.com>
2024-10-18 15:20:27 -0700

    who knows it might just work :)

---
+++ index.js
@@ -0,0 +1,5 @@
+
+window.onload = function() {
+	//document.getElementById("arrow-container").classList.add("arrow-transition");
+	//	document.getElementById("pre_order_a").classList.add("pre-order-reveal");
+};
